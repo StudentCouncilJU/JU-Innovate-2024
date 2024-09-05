9c6e137c2116109bf56bd8b4aa6f22b8b3ecd965
MICHAEL PAUL <118343963+Michael-Paul347@users.noreply.github.com>
2024-09-05 16:02:49 +0530

    ir change

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -57,14 +57,14 @@ const sponsors = () => {
                 <div className="mx-auto  px-2 py-2 lg:px-10 lg:pt-12 ">
                     <div className="container justify-center lg:max-w-[1300px]">
                         <div className="grid  grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4">
-                            {eventSponsors.map((sponsor) => (
+                            {/* {eventSponsors.map((sponsor) => (
                                 <div className=' p-3 justify-center flex rounded-3xl m-4'>
                                     <div>
                                         <Image className="h-auto max-w-full  rounded-xl transition-transform duration-300 transform hover:scale-105 cursor-pointer" width={300} height={300} src={sponsor.image} alt="" />
                                        
                                     </div>
                                 </div>
-                            ))}
+                            ))} */}
                           
                         </div>
                         <h1 className='text-white text-7xl text-center my-auto'>Coming Soon</h1>
